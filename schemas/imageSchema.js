f1ccd769e6ee74041149cb619ae54d7f09825f9b
Jannis Lehmann <cludch@gmail.com>
2017-05-01 21:43:06 +0200

    I want TypeScript

--- schemas/imageSchema.js
+++ schemas/imageSchema.js
@@ -24,7 +24,7 @@ module.exports = () => {
     };
 
     const imageSchema = new mongoose.Schema(imageSchemaArray);
-    schema.plugin(ttl, { ttl: config.expiresAt });
+    imageSchema.plugin(ttl, { ttl: config.expiresAt });
 
     databaseManager.addSchema('Image', imageSchema);
 };
